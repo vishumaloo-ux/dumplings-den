e17bfcd893a625d10a42d77fc3089229977e6a1d
vishumaloo-ux <vishumaloo@gmail.com>
2025-08-26 22:53:02 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Welcome to Dumpling's Den 🐾</h1>
+      <p>A shop for Shih Tzu lovers! (63 days of cuteness)</p>
+    </div>
+  );
+}
